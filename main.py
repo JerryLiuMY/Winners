a4e5dabe3fb9feb6032c5cb7b667dc0bf78baf93
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-04 11:59:37 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from models.conditional import get_truncation
-from models.conditional import search_mu
+from models.winners import get_truncation
+from models.winners import search_mu
 ltilde, utilde = get_truncation()
 mu_estimate = search_mu(ltilde, utilde, size=0.5)
