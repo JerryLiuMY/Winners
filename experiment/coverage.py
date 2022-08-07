db8c6f1f77de3f3342a89eb00286e983bc11479f
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-08-07 16:25:36 -0500

    update

--- experiment/coverage.py
+++ experiment/coverage.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from data_prep.data_prep import data_prep
+from data_prep.dgp import DGP
 from models.winners import Winners
 from models.naive import Naive
 from datetime import datetime
@@ -8,35 +8,36 @@ import numpy as np
 sns.set()
 
 
-def find_coverage(model_name, ntrials, narms, diff):
+def find_coverage(model_name, ntrials, nsamples, narms, mu, cov):
     """ RUn experiment
     :param model_name: Model name
     :param ntrials: Number of trials
+    :param nsamples: Number of samples
     :param narms: number of treatment
-    :param diff: difference between winning arm and the remaining arms
+    :param mu: mean of the data generation
+    :param cov: covariance of the data generation
     :return: coverage rate
     """
 
-    # generate data
+    # define parameters
     tol = 1e-5
-    Y_all, sigma = data_prep(ntrials, narms, diff)
     model_dict = {"Naive": Naive, "Winners": Winners}
     Model = model_dict[model_name]
 
     # find coverage rate
     coverage_li = []
-    for idx, Y in enumerate(Y_all):
-        # logging massage
+    for idx in np.range(ntrials):
         if idx % 100 == 0:
-            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Working on sample {idx}")
+            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Working on trial {idx}")
 
-        # logging massage
+        # search for mu
+        Y, sigma = DGP(nsamples, narms, mu, cov).get_input()
         model = Model(Y, sigma)
         mu_lower = model.search_mu(alpha=0.025, tol=tol)
         mu_upper = model.search_mu(alpha=1-0.025, tol=tol)
 
         # append coverage rate
-        coverage_li.append((diff > mu_upper) & (diff < mu_lower))
+        coverage_li.append((np.max(mu) > mu_upper) & (np.max(mu) < mu_lower))
 
     return np.mean(coverage_li)
 
@@ -51,14 +52,16 @@ def plot_coverage(model_name, ntrials):
     # define parameters
     diff_li = list(np.arange(0, 8 + 0.5, 0.5))
     narms_li = [2, 10, 50]
+    nsamples_li = [_ * 50 for _ in narms_li]
 
     # get coverage rate
     coverage_arr = np.empty(shape=(len(diff_li), len(narms_li)))
     for i, diff in enumerate(diff_li):
-        for j, narms in enumerate(narms_li):
+        for j, (nsamples, narms) in enumerate(zip(nsamples_li, narms_li)):
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
                   f"Working on difference = {diff} and number of arms = {narms}")
-            coverage_arr[i, j] = find_coverage(model_name, ntrials, narms, diff)
+            mu, cov = np.arange(narms) - (narms - 1), np.ones(narms)
+            coverage_arr[i, j] = find_coverage(model_name, ntrials, nsamples, narms, mu, cov)
     coverage_arr = np.round(coverage_arr, 2)
 
     # plot coverage rate
--- experiment/power.py
+++ experiment/power.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from data_prep.data_prep import data_prep
+from data_prep.dgp import DGP
 from models.winners import Winners
 from models.naive import Naive
 from datetime import datetime
@@ -8,31 +8,32 @@ import numpy as np
 sns.set()
 
 
-def find_power(model_name, ntrials, narms, diff, null_li):
+def find_power(model_name, ntrials, nsamples, narms, mu, cov, null_li):
     """ RUn experiment
     :param model_name: Model name
     :param ntrials: Number of trials
+    :param nsamples: Number of samples
     :param narms: number of treatment
-    :param diff: difference between winning arm and the remaining arms
+    :param mu: mean of the data generation
+    :param cov: covariance of the data generation
     :param null_li: List of null hypothesis
     :return: coverage rate
     """
 
-    # generate data
-    Y_all, sigma = data_prep(ntrials, narms, diff)
+    # define parameters
     model_dict = {"Naive": Naive, "Winners": Winners}
     Model = model_dict[model_name]
 
     # find coverage rate
     power_li = []
     for idx, null in enumerate(null_li):
-        # logging massage
         if idx % 100 == 0:
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Working on null hypothesis {idx}")
 
-        # find power corresponding to the null
+        # find power
         power_li_sub = []
-        for Y in Y_all:
+        for _ in np.range(ntrials):
+            Y, sigma = DGP(nsamples, narms, mu, cov).get_input()
             model = Model(Y, sigma)
             power_li_sub.append(model.get_power(null=null))
         power_li.append(np.mean(power_li_sub))
@@ -50,16 +51,18 @@ def plot_power(model_name, ntrials):
     # define parameters
     diff_li = [1, 2, 3, 4]
     narms_li = [2, 10, 50]
+    nsamples_li = [_ * 50 for _ in narms_li]
 
     # plot power
     fig, axes = plt.subplots(2, 2, figsize=(12, 12))
     for i, diff in enumerate(diff_li):
         ax = axes[i // 2, i % 2]
         null_li = np.linspace(diff - 5, diff + 5, 101)
-        for j, narms in enumerate(narms_li):
+        for j, (nsamples, narms) in enumerate(zip(nsamples_li, narms_li)):
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
                   f"Working on difference = {diff} and number of arms = {narms}")
-            power_li = find_power(model_name, ntrials, narms, diff, null_li)
+            mu, cov = np.arange(narms) - (narms - 1), np.ones(narms)
+            power_li = find_power(model_name, ntrials, nsamples, narms, mu, cov, null_li)
             ax.plot(power_li, "-", label=f"narms={narms}")
 
         ax.set_xticks([val for idx, val in enumerate(np.arange(len(null_li))) if idx % 10 == 0])
